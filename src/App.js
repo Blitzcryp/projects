882da4eb98c93e498b159713c90ccc8f341b1f50
B̴l̷i̷t̶x̷ <59416533+Blitzcryp@users.noreply.github.com>
2022-01-02 16:28:20 +0200

    Try to fix vercel warning issue

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import { Container, Row } from 'react-bootstrap';
 import Navbar from './components/Card.js/navbar/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 
-import ReverseAString from './projects/ReverseAString';
-
-const projects = [
-  {
-      name: "Reverse a String",
-      description: "Enter a string and the program will reverse it and print it out.",
-      link: "reverse-a-string",
-      element: <ReverseAString/>
-  },
-  {
-    name: "Pig Latin",
-    description: ` Pig Latin is a game of alterations played on the English language game. To create the Pig Latin form of an English word the initial consonant sound is transposed to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay). Read Wikipedia for more information on rules.`,
-    link: "pig-latin",
-  },
-]
+import projects from "./utils/projects";
 
 function App() {
   let projectRenderCards = [];
@@ -40,7 +26,6 @@ function App() {
         <Routes>
           <Route path="/" element={ projectRenderCards } />
           { projectsRenderRoutes }
-          <Route path="reverse-a-string" element={<ReverseAString />} />
         </Routes>
       </Row>
     </Container>
